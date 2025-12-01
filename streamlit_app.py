235ed8d1ee3d4463a16ed20ea31cba06eb8fc136
thescmguy <thescmguy@gmail.com>
2025-12-01 17:21:23 +0530

    Added search_on column to dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ session = cnx.session()
 name_on_order = st.text_input('Name on smoothie:')
 st.write('The name on your smoothie will be : ', name_on_order)
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients', my_dataframe, max_selections=5)
